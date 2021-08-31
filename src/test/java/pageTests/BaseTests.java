289941446688e480a3290559aaaf1483e65c9da1
hakanakc <akkayahakan3448@gmail.com>
2021-08-31 23:53:50 +0100

    31August

--- src/test/java/pageTests/BaseTests.java
+++ src/test/java/pageTests/BaseTests.java
@@ -1,5 +1,8 @@
 package pageTests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -16,10 +19,19 @@ public WebDriver driver;
 
 public WebDriverWait wait;
 
+public ExtentReports report;
+public ExtentHtmlReporter htmlReporter;
+public ExtentTest test;
+
+
+
 @BeforeTest
     public void beforeTest(){
 
     //Reports setup
+
+    report = new ExtentReports();
+    htmlReporter=
 
 }
 @BeforeMethod
